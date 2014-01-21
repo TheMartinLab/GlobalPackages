ef6d1c2f4e569f15616cfb2cff9a5abeb6622ad9
Eric Dill <eddill@ncsu.edu>
2014-01-20 19:32:36 -0500

    Signed-off-by: Eric Dill <eddill@ncsu.edu>

---
+++ FileFormats/PDB.java
@@ -0,0 +1,22 @@
+
+public class PDB {
+
+	/** MANDATORY */
+	private String HEADER = "HEADER";
+	private String TITLE  = "TITLE";
+	private String COMPND = "COMPND";
+	private String SOURCE = "SOURCE";
+	private String KEYWDS = "KEYWDS";
+	private String EXPDTA = "EXPDTA";
+	private String AUTHOR = "AUTHOR";
+	private String REVDAT = "REVDAT";
+	
+	/** OPTIONAL */
+	private String OBSLTE = "OBSLTE";
+	private String SPLIT  = "SPLIT";
+	private String CAVEAT = "CAVEAT";
+	private String NUMDL  = "NUMDL";
+	private String MDLTYP = "MDLTYP";
+	
+	
+}
